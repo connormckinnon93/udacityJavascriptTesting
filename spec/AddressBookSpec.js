2d52ded573f23672e40762580debec1e67d59757
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-17 08:57:00 -0400

    refactor code to isolate before each process

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,17 +1,19 @@
 describe('Address Book', function() {
+	var addressBook,
+		thisContact;
+	
+	beforeEach(function() {
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+	});
+	
 	it('should be able to add a contact', function() {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
-		
 		addressBook.addContact(thisContact);
 		
 		expect(addressBook.getContact(0)).toBe(thisContact);
 	});
 	
 	it('should be able to delte a contact', function() {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
-		
 		addressBook.addContact(thisContact);
 		addressBook.deleteContact(0);
 		
